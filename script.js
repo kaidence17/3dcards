d3627ac4b0b046d5cc8dd727caf1c93fb3832f6c
Kaidence <123508047+kaidence17@users.noreply.github.com>
2023-04-25 21:24:59 +0000

    fonts

--- script.js
+++ script.js
@@ -6,7 +6,4 @@ document.querySelectorAll('.card').forEach( item => {
     
     // for each item i.e. card, do this
     item.addEventListener('click', event => {
-      
-      // toggle class on card
-      item.classList.toggle('flipped');
-    })
+      item.classList.toggle('flipped');}
