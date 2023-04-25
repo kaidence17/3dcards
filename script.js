b64faa3719a8023b14ae59e539198dcce1c43da9
Kaidence <123508047+kaidence17@users.noreply.github.com>
2023-04-25 21:17:13 +0000

    print page

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // scripts
 
 console.log('ok!'); 
+
+document.querySelectorAll('.card').forEach( item => {
+    
+    // for each item i.e. card, do this
+    item.addEventListener('click', event => {
+      
+      // toggle class on card
+      item.classList.toggle('flipped');
+    })
